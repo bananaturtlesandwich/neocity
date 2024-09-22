bd88db6806649598ec4ce6f5809b779a81009297
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2024-09-22 15:43:39 +0100

    finish blue fire articles

--- book.js
+++ book.js
@@ -1,4 +1,8 @@
 function turn(left, right, header = true) {
-  document.getElementById('left').innerHTML = header ? `<u>${left}</u><br>` + document.getElementById(left).innerHTML : document.getElementById(left).innerHTML;
-  document.getElementById('right').innerHTML = header ? `<u>${right}</u><br>` + document.getElementById(right).innerHTML : document.getElementById(right).innerHTML;
+  let leftpage = document.getElementById('left');
+  leftpage.innerHTML = header ? `<u>${left}</u><br>` + document.getElementById(left).innerHTML : document.getElementById(left).innerHTML;
+  leftpage.scrollTop = 0;
+  let rightpage = document.getElementById('right');
+  rightpage.innerHTML = header ? `<u>${right}</u><br>` + document.getElementById(right).innerHTML : document.getElementById(right).innerHTML;
+  rightpage.scrollTop = 0;
 }
