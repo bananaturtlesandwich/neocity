0fdc7af0bd0cc190d0cc70a9d50aa2b997169c02
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2024-12-12 22:48:09 +0000

    transfer logs to main

--- tag.js
+++ tag.js
@@ -1,6 +1,8 @@
 function filter(tag) {
   for (const element of document.querySelectorAll("details")) {
-    element.hidden = tag != "all" && !element.getAttribute('tags').includes(tag);
+    const tags = element.getAttribute('tags');
+    if (tags == null) continue;
+    element.hidden = tag != "all" && !tags.includes(tag);
   }
   document.getElementById("desc").innerHTML = document.getElementById(tag).innerHTML;
 }
