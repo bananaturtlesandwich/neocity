ea0b10be91d625c87f3f02229942175d9332c3a8
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2024-12-23 16:43:01 +0000

    add art page

--- tag.js
+++ tag.js
@@ -1,8 +1,8 @@
 function filter(tag) {
-  for (const element of document.querySelectorAll("details")) {
+  for (const element of document.querySelectorAll("[tags]")) {
     const tags = element.getAttribute('tags');
-    if (tags == null) continue;
-    element.hidden = tag != "all" && !tags.includes(tag);
+    element.hidden = !(tag == "all" || tags.includes(tag));
   }
-  document.getElementById("desc").innerHTML = document.getElementById(tag).innerHTML;
+  const desc = document.getElementById("desc");
+  if (desc != null) desc.innerHTML = document.getElementById(tag).innerHTML;
 }
