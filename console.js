5b8ad3e5a62f74eacd5806e9e5507efed6874677
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2025-02-03 17:11:56 +0000

    start work on modding shrine

---
+++ console.js
@@ -0,0 +1,17 @@
+function input(event) {
+  const command = document.getElementById("console");
+  command.style.width = (command.value.length) + 'ch';
+}
+
+function submit() {
+  const console = document.getElementById("console");
+  const command = document.getElementById("command");
+  console.innerHTML += "> " + command.value + "<br>"
+  const value = command.value.trim();
+  if (value == "cls") console.innerHTML = ""
+  else {
+    const text = document.getElementById(value);
+    console.innerHTML += text == null ? "not a valid command<br>" + document.getElementById("help").innerHTML : text.innerHTML
+  }
+  command.value = "";
+}
