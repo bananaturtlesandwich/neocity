3e4484e05ce0419079d23389fd0afd5847184e40
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2025-04-15 17:17:25 +0100

    workaround for now

---
+++ onionring-variables.js
@@ -0,0 +1,43 @@
+// onionring.js is made up of four files - onionring-widget.js, onionring-index.js, onionring-variables.js (this one!), and onionring.css
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (è’œ) house, last updated 2020-11-24
+
+// === ONIONRING-VARIABLES ===
+//this file contains the stuff you edit to set up your specific webring
+
+//the full URLs of all the sites in the ring
+var sites = [
+    'https://compulsivecomputer.net/',
+    'https://grulovia.neocities.org/',
+    'https://catboggy.neocities.org/',
+    'https://metonym.neocities.org/',
+    'https://indygiertz.net',
+    'https://cawsmicentity.neocities.org/',
+    'https://alongcomesmary.neocities.org/',
+    'https://johndoe24.neocities.org/',
+    'https://unitesynchronization.neocities.org/',
+    'https://parallel1cortex.neocities.org/',
+    'https://on-bambis-mind.neocities.org/',
+    'https://ninacti0n.art/',
+    'https://spuds.neocities.org/',
+    'https://flooreo.neocities.org/',
+    'https://confetticann0n.neocities.org/'
+    ];
+    
+
+//the name of the ring
+var ringName = 'Collective Unconscious Webring';
+
+/* the unique ID of the widget. two things to note:
+ 1) make sure there are no spaces in it - use dashes or underscores if you must
+ 2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
+var ringID = 'PNwebring';
+
+//should the widget include a link to an index page?
+var useIndex = true;
+//the full URL of the index page. if you're not using one, you don't have to specify anything here
+var indexPage = 'https://compulsivecomputer.net/CollectiveUnconscious.html';
+
+//should the widget include a random button?
+var useRandom = false;
+
