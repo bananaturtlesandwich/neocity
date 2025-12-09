3023b6c71dcfddae2a7431a9cab4256592f8c7c3
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2025-12-09 18:36:24 +0000

    automate husking

---
+++ husk.bat
@@ -0,0 +1,19 @@
+for /r %%g in (*.html) do (
+  echo ^<!DOCTYPE html^> >%%g
+  echo ^<head^> >>%%g
+  echo   ^<meta http-equiv=^"refresh^" content=^"0; url=https://spuds.casa/%%~ng^"^> >>%%g
+  echo ^</head^> >>%%g
+  echo ^</html^>" >>%%g
+  move %%g src/%%g
+)
+for /r %%g in (*.ico,*.png,*.jpg,*.jpeg,*.webp,*.gif,*.ase,*.css,*.js) do del "%%g"
+
+mkdir src
+for /d %%g in (*) do (
+  move %%g %%~dpgsrc\%%~nxg
+)
+for %%g in (*) do (
+  (echo "%%g" | find /i "husk.bat" 1>NUL) || (  
+    move %%g %%~dpgsrc\%%~nxg
+  )
+)
