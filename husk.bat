20e2d051f10eb21eb661c9d2237bf38ca20012b5
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2025-12-13 00:34:40 +0000

    improve husking

--- husk.bat
+++ husk.bat
@@ -4,7 +4,6 @@ for /r %%g in (*.html) do (
   echo   ^<meta http-equiv=^"refresh^" content=^"0; url=https://spuds.casa/%%~ng^"^> >>%%g
   echo ^</head^> >>%%g
   echo ^</html^>" >>%%g
-  move %%g src/%%g
 )
 for /r %%g in (*.ico,*.png,*.jpg,*.jpeg,*.webp,*.gif,*.ase,*.css,*.js) do del "%%g"
 
