18e3d570cb8322008e5f9262825d015bc24779fc
spuds <71292624+bananaturtlesandwich@users.noreply.github.com>
2026-02-12 15:37:36 +0000

    ramble about my game

--- popup.js
+++ popup.js
@@ -1,13 +1,10 @@
 function popup(id) {
   const popup = document.getElementById('popup');
+  popup.innerHTML = document.getElementById(id).innerHTML;
   popup.hidden = false;
   popup.scrollTop = 0;
 }
 
 function pop() {
   document.getElementById('popup').hidden = false;
-}
-
-function dismiss() {
-  document.getElementById('popup').hidden = true;
 }
